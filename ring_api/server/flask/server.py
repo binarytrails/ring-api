1e6b38066b9a0c6b12470a469fc1727f3c8e1c10
Seva Ivanov <mail@sevaivanov.com>
2016-07-04 20:12:15 -0400

    server: fix runtime failure

--- ring_api/server/flask/server.py
+++ ring_api/server/flask/server.py
@@ -45,7 +45,7 @@ class FlaskServer:
             '/accounts/<account_id>/call/',
             resource_class_kwargs={'dring': self.dring})
         
-        self.api.add_resource(account.AccountsCertifificates,
+        self.api.add_resource(account.AccountsCertificates,
             '/accounts/<account_id>/certificates/<cert_id>/',
             resource_class_kwargs={'dring': self.dring})
 
